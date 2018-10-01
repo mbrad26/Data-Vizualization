32218eae6c6489659a25adaf92b38183c7b3c837
Marius Brad <mbrad26@gmail.com>
2018-10-01 12:11:30 +0100

    ..

--- CSV File Vizualization/highs_lows.py
+++ CSV File Vizualization/highs_lows.py
@@ -1,1 +1,13 @@
+import csv
+
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    # header_row = next(reader)
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+    highs = []
+    for row in reader:
+        high = row[1]
+        print(high)
 
