72c9363f841137d934bbe237144f28349f088502
Marius Brad <mbrad26@gmail.com>
2018-10-02 18:26:55 +0100

    CSV and JSON work

--- CSV File Vizualization/sitka_death_valley_rainfall.py
+++ CSV File Vizualization/sitka_death_valley_rainfall.py
@@ -1,17 +1,36 @@
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pygal
 
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header = next(reader)
-    print(reader)
-    for index, name in enumerate(reader):
-        print(index, name)
+    # for index, name in enumerate(reader):
+    #     print(index, name)
 
     dates, precips = [], []
     for row in reader:
-        date = row[0]
-        precip = row[19]
+        date = datetime.strptime(row[0], '%Y-%m-%d')
+        precip = int(float(row[19]))
 
         dates.append(date)
         precips.append(precip)
+
+# plt.plot()
+fig = plt.figure(figsize=(10, 6))
+plt.plot(dates, precips, c='red', alpha=.7)
+plt.title('Sitka - Death Valley rainfall comparison -2014', fontsize=26)
+plt.xlabel('', fontsize=14)
+plt.ylabel('Precipitations', fontsize=14)
+plt.tick_params(axis='both', labelsize=14)
+fig.autofmt_xdate()
+plt.savefig('images/sitka_valley_rainfall_2014.png', bbox_inches='tight')
+plt.show()
+
+# pygal.Bar()
+hist = pygal.Bar()
+hist.title = 'Sitka - Death Valley rainfall comparison -2014'
+hist.add('2014', precips)
+hist.render_to_file('images/sitka_valley_rainfall_2014.svg')
